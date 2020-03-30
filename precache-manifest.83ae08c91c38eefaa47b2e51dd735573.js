101b981f05508dc36ec6f55b6d013e824cdd1b3d
Sam Hall <sam_rm@icloud.com>
2020-03-30 09:29:31 +0100

    Updates

---
+++ precache-manifest.83ae08c91c38eefaa47b2e51dd735573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94418ce525744ea2f5f9579b0d161540",
+    "url": "/First-react-website/index.html"
+  },
+  {
+    "revision": "c3dd05c2d45a1dfbb21a",
+    "url": "/First-react-website/static/css/main.171aa99c.chunk.css"
+  },
+  {
+    "revision": "08a60d35447e251fd039",
+    "url": "/First-react-website/static/js/2.f2ff127e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/First-react-website/static/js/2.f2ff127e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3dd05c2d45a1dfbb21a",
+    "url": "/First-react-website/static/js/main.7a3883e1.chunk.js"
+  },
+  {
+    "revision": "934735d889daa548391e",
+    "url": "/First-react-website/static/js/runtime-main.b70ad9d8.js"
+  }
+]);
